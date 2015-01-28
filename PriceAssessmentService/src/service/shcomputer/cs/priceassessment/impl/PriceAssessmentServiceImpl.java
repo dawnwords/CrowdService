3fa1758270eed558e1cea27d720de4e1d8e9693e
Nicolas <jiahuanzheng@gmail.com>
2015-01-28 15:18:24 +0800

    + PriceAssessmentService

---
+++ PriceAssessmentService/src/service/shcomputer/cs/priceassessment/impl/PriceAssessmentServiceImpl.java
@@ -0,0 +1,43 @@
+package service.shcomputer.cs.priceassessment.impl;
+
+import edu.fudan.se.crowdservice.core.ConcreteService;
+import edu.fudan.se.crowdservice.core.CrowdServiceStub;
+import edu.fudan.se.crowdservice.kv.KeyValueHolder;
+import service.shcomputer.cs.priceassessment.interfaces.PriceAssessmentService;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jiahuan on 2015/1/27.
+ */
+public class PriceAssessmentServiceImpl extends ConcreteService implements PriceAssessmentService {
+    public static final String URL = "http://10.131.253.172:8887/priceassess?wsdl";
+    public static final String NS = "http://priceassessment.crowdservice.se.fudan.edu/";
+    public static final String METHOD = "assessPrice";
+
+
+    @Override
+    public ArrayList<KeyValueHolder> assessPrice(String brand, String series, String newness, String CPU, String memory, String hardDisk, String imagePath) {
+        CrowdServiceStub stub = new CrowdServiceStub(URL, NS, METHOD, time, this);
+
+        stub.addProperty("arg0", consumerId);
+        stub.addProperty("arg1", cost);
+        stub.addProperty("arg2", time);
+        stub.addProperty("arg3", templateName);
+        stub.addProperty("arg4", brand);
+        stub.addProperty("arg5", series);
+        stub.addProperty("arg6", newness);
+        stub.addProperty("arg7", CPU);
+        stub.addProperty("arg8", memory);
+        stub.addProperty("arg9", hardDisk);
+        stub.addProperty("arg10", loadDataAsBase64ByPath(imagePath));
+
+        return stub.sendSOAP();
+    }
+
+    @Override
+    protected Class getServiceInterface() {
+        return PriceAssessmentService.class;
+    }
+
+}
---
+++ PriceAssessmentService/src/service/shcomputer/cs/priceassessment/interfaces/PriceAssessmentService.java
@@ -0,0 +1,12 @@
+package service.shcomputer.cs.priceassessment.interfaces;
+
+import edu.fudan.se.crowdservice.kv.KeyValueHolder;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jiahuan on 2015/1/27.
+ */
+public interface PriceAssessmentService {
+    ArrayList<KeyValueHolder> assessPrice(String brand, String series, String newness, String CPU, String memory, String hardDisk, String imagePath);
+}
