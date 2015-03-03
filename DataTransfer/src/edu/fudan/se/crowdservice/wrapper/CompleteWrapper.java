e139c8e2619af571c2fdc6bd61a58414d4aaefb5
Gu Jingxiao <gujingxiao@gmail.com>
2015-03-03 19:57:10 +0800

    + CompleteWrapper

---
+++ DataTransfer/src/edu/fudan/se/crowdservice/wrapper/CompleteWrapper.java
@@ -0,0 +1,12 @@
+package edu.fudan.se.crowdservice.wrapper;
+
+/**
+ * Created by Dawnwords on 2015/3/3.
+ */
+public class CompleteWrapper extends Wrapper {
+    private static final long serialVersionUID = -8672514790738845223L;
+
+    public CompleteWrapper(long taskId) {
+        super(taskId);
+    }
+}
