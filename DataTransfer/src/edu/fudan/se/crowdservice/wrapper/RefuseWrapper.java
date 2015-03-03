ac4bd7eaa045107e99ad9804249295d24aecf769
Gu Jingxiao <gujingxiao@gmail.com>
2015-03-03 19:56:39 +0800

    + enum Reason in RefuseWrapper

--- DataTransfer/src/edu/fudan/se/crowdservice/wrapper/RefuseWrapper.java
+++ DataTransfer/src/edu/fudan/se/crowdservice/wrapper/RefuseWrapper.java
@@ -5,11 +5,20 @@ package edu.fudan.se.crowdservice.wrapper;
  */
 public class RefuseWrapper extends Wrapper {
     private static final long serialVersionUID = -1774891502313329253L;
-
     public final String reason;
 
-    public RefuseWrapper(long taskId, String reason) {
+    public RefuseWrapper(long taskId, Reason reason) {
         super(taskId);
-        this.reason = reason;
+        this.reason = reason.reason;
+    }
+
+    public static enum Reason {
+        OFFER_NOT_SELECTED("Offer Not Selected"), OFFER_OUT_OF_DATE("Offer Out of Date");
+
+        public final String reason;
+
+        Reason(String reason) {
+            this.reason = reason;
+        }
     }
 }
