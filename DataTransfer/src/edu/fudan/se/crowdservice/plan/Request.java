08930c1aeced5fc4fc83e13f1a990f35a51fe2bc
Gu Jingxiao <gujingxiao@gmail.com>
2015-03-08 14:34:48 +0800

    + Request & Response in DataTransfer + Gson lib

---
+++ DataTransfer/src/edu/fudan/se/crowdservice/plan/Request.java
@@ -0,0 +1,45 @@
+package edu.fudan.se.crowdservice.plan;
+
+import java.util.Map;
+
+/**
+ * Created by Dawnwords on 2015/3/8.
+ */
+public class Request {
+    private int globalTime;
+    private int globalCost;
+    private Map<String,Integer> resultNumbers;
+    private String templateName;
+
+    public int getGlobalTime() {
+        return globalTime;
+    }
+
+    public void setGlobalTime(int globalTime) {
+        this.globalTime = globalTime;
+    }
+
+    public int getGlobalCost() {
+        return globalCost;
+    }
+
+    public void setGlobalCost(int globalCost) {
+        this.globalCost = globalCost;
+    }
+
+    public Map<String, Integer> getResultNumbers() {
+        return resultNumbers;
+    }
+
+    public void setResultNumbers(Map<String, Integer> resultNumbers) {
+        this.resultNumbers = resultNumbers;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+}
---
+++ DataTransfer/src/edu/fudan/se/crowdservice/plan/Response.java
@@ -0,0 +1,34 @@
+package edu.fudan.se.crowdservice.plan;
+
+/**
+ * Created by Dawnwords on 2015/3/8.
+ */
+public class Response {
+    private double globalReliability;
+    private int time;
+    private int cost;
+
+    public double getGlobalReliability() {
+        return globalReliability;
+    }
+
+    public void setGlobalReliability(double globalReliability) {
+        this.globalReliability = globalReliability;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
